492645e9134ad1a1f131d04c4ea64606c7c09b2a
alflsftalttbyqyt-crypto <alflsftalttbyqyt@gmail.com>
2026-08-11 20:07:14 +0100

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1525,5 +1525,6 @@ const handleResetPassword = async () => {
             جميع المقررات والملخصات تخضع لملكية أساتذة وطلبة الكلية متعددة التخصصات بالرشيدية.
           </div>
         </div>
+      )
 }
 export default App;
